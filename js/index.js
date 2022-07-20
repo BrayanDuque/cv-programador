1d1e9be60a7443d867fab3a6b83134d0cdb6cbb4
brayan duque <bradumar2016@gmail.com>
2022-07-20 02:48:55 -0500

    cv-programador

---
+++ js/index.js
@@ -0,0 +1,18 @@
+let numero = document.getElementById("numero");
+const dedo = document.getElementById("like")
+const fragment = document.createDocumentFragment();
+
+
+
+dedo.addEventListener("click",()=>{
+    cantidad = 10;
+    numero.innerHTML = cantidad + 1
+
+    dedo.classList.add("text-primary")
+})
+
+
+dedo.addEventListener("dblclick",()=>{
+    numero.innerHTML = cantidad 
+    dedo.classList.remove("text-primary")
+})
