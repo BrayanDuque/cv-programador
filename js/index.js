107321f1f98843ba5431ede2691b0a045baf3ec7
brayan duque <bradumar2016@gmail.com>
2022-07-21 01:10:51 -0500

    manipulando el dom

--- js/index.js
+++ js/index.js
@@ -1,19 +1,22 @@
 let numero = document.getElementById("numero");
 const dedo = document.getElementById("like")
-const fragment = document.createDocumentFragment();
+// const fragment = document.createDocumentFragment();
 
 
 
 dedo.addEventListener("click",()=>{
-    cantidad = 10;
-    numero.innerHTML = cantidad + 1
+    let cantidad = 10;
 
-
+    cantidad++;
+    numero.value = cantidad;
+    numero.textContent = numero.value;
+    
+})
+dedo.addEventListener("click",()=>{
     dedo.classList.add("text-primary")
 })
 
-
 dedo.addEventListener("dblclick",()=>{
-    numero.innerHTML = cantidad 
+    numero.textContent = numero.value - 1
     dedo.classList.remove("text-primary")
 })
